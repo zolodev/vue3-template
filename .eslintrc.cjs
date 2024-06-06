ed7b221fa64077e6f671a8767a4e5c7ae689d9e8
Zolo <39656359+zolodev@users.noreply.github.com>
2024-06-06 23:04:07 +0200

    init

---
+++ .eslintrc.cjs
@@ -0,0 +1,24 @@
+/* eslint-env node */
+require('@rushstack/eslint-patch/modern-module-resolution')
+
+module.exports = {
+  root: true,
+  extends: [
+    'plugin:vue/vue3-essential',
+    'eslint:recommended',
+    '@vue/eslint-config-typescript',
+    '@vue/eslint-config-prettier/skip-formatting',
+    'plugin:vue/vue3-strongly-recommended',
+    'plugin:vue/vue3-recommended'
+  ],
+  parserOptions: {
+    ecmaVersion: 'latest'
+  },
+  rules: {
+    // override/add rules settings here, such as:
+    // 'vue/no-unused-vars': 'error'
+  },
+  globals: {
+    _: true
+  }
+}
