d34e9e4fcc0339cb4899108549508741644763c4
Zolo <39656359+zolodev@users.noreply.github.com>
2024-06-12 20:21:26 +0200

    updating eslint rules

--- .eslintrc.cjs
+++ .eslintrc.cjs
@@ -17,6 +17,7 @@ module.exports = {
   rules: {
     // override/add rules settings here, such as:
     // 'vue/no-unused-vars': 'error'
+    'vue/multi-word-component-names': 0
   },
   globals: {
     _: true
