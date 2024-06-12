3fa60cf05e5968c28b388e560339ea7872ee3fd8
Zolo <39656359+zolodev@users.noreply.github.com>
2024-06-12 19:46:22 +0200

    adding unplugin vue router

--- src/router/index.ts
+++ src/router/index.ts
@@ -1,21 +1,9 @@
-import HomeView from '@/views/HomeView.vue'
-import { createRouter, createWebHistory } from 'vue-router'
+import { createRouter, createWebHistory } from 'vue-router/auto'
+import { routes } from 'vue-router/auto-routes'
 
 const router = createRouter({
   history: createWebHistory(import.meta.env.BASE_URL),
-  routes: [
-    {
-      path: '/',
-      name: 'root',
-      component: HomeView
-    }
-    // Example dynamic route
-    // {
-    //   path: '/another',
-    //   name: 'another',
-    //   component: () => import('@/views/AnotherView.vue')
-    // }
-  ]
+  routes
 })
 
 export default router
--- vite.config.ts
+++ vite.config.ts
@@ -1,14 +1,18 @@
 import { fileURLToPath, URL } from 'node:url'
+import VueRouter from 'unplugin-vue-router/vite'
 
-import { defineConfig } from 'vite'
 import vue from '@vitejs/plugin-vue'
+import { defineConfig } from 'vite'
 import VueDevTools from 'vite-plugin-vue-devtools'
 
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [
+    VueRouter({
+      /* options */
+    }),
     vue(),
-    VueDevTools(),
+    VueDevTools()
   ],
   resolve: {
     alias: {
